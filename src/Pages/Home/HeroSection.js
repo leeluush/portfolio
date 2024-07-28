adeb9aa63482a9f1ad76aeb4ebddab5c896bf34e
Lee Confino <leeluush@gmail.com>
2024-07-28 17:21:15 +0300

    readme file

--- src/Pages/Home/HeroSection.js
+++ src/Pages/Home/HeroSection.js
@@ -6,11 +6,15 @@ import 'wired-elements';
 
 export default function HeroSection() {
   const { theme } = useTheme();
-  const [heroImageSrc, setHeroImageSrc] = useState(theme.palette.mode === 'dark' ? './img/meDrawLampOn.png' : './img/meDrawWithLamp.png');
+  const isDarkMode = theme.palette.mode === 'dark';
+  const heroImageSrc = isDarkMode ? './img/meDrawLampOn.png' : './img/meDrawWithLamp.png';
+  const placeholderImageSrc = isDarkMode ? './img/meDrawLampOn-small.png' : './img/meDrawWithLamp-small.png';
 
-  useEffect(() => {
-    setHeroImageSrc(theme.palette.mode === 'dark' ? './img/meDrawLampOn.png' : './img/meDrawWithLamp.png');
-  }, [theme.palette.mode]);
+  const [loaded, setLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setLoaded(true);
+  }
 
   return (
     <section id="heroSection" className="hero--section" style={{ backgroundColor: theme.palette.background.default }}>
@@ -20,7 +24,7 @@ export default function HeroSection() {
           className="hero--section-content-card"
           sx={{ 
             padding: '20px', 
-            maxWidth: '600px',  // Set maximum width for the text box
+            maxWidth: '600px',  
             backgroundColor: theme.palette.background.paper, 
             borderColor: theme.palette.mode === 'dark' ? '#555' : '#ccc',
             borderWidth: '1px',
@@ -49,48 +53,52 @@ export default function HeroSection() {
             <Typography 
               variant="body1" 
               className="hero--section-description" 
-              style={{ color: theme.palette.text.primary, fontFamily: 'Caveat, cursive', fontSize: '18px' }}
+              style={{ color: theme.palette.text.primary, fontFamily: 'Caveat, cursive', fontSize: '18px', lineHeight: '1.5' }}
             >
               I develop seamless, robust, and intuitive web applications that bring your digital visions to life. With a passion for both front-end and back-end technologies, I craft comprehensive solutions that enhance user interaction and deliver functionality efficiently. Whether you're looking to build from scratch or refine an existing project, I'm here to help you make a significant impact online.
             </Typography>
-            <ScrollLink to="contactSection" spy={true} smooth={true} offset={-70} duration={500}>
-              <Button 
-                variant="contained" 
-                className="btn-primary" 
-                sx={{
-                  marginTop: '20px',
-                  height: '36px',
-                  width: '160px', // Increased width for better visibility
-                  backgroundColor: 'var(--highlight-color)', // Set the initial button color
-                  color: 'var(--button-color)',
-                  border: '1px solid black',
-                  borderRadius: '5px',
-                  cursor: 'pointer',
-                  transition: 'background-color 0.3s, box-shadow 0.3s',
-                  fontFamily: 'Caveat, cursive',
-                  fontSize: '18px',
-                  boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
-                  '&:hover': {
-                    backgroundColor: '#7BBAFF', 
-                    boxShadow: '0px 6px 12px rgba(0, 0, 0, 0.3)', // More pronounced shadow on hover
-                  },
-                  textTransform: 'none', // Ensure text case matches design
-                }}
-              >
-                GET IN TOUCH
-              </Button>
-            </ScrollLink>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <ScrollLink to="contactSection" spy={true} smooth={true} offset={-70} duration={500}>
+                <Button 
+                  variant="contained" 
+                  className="btn-primary" 
+                  sx={{
+                    marginTop: '20px',
+                    height: '36px',
+                    width: '160px',
+                    backgroundColor: 'var(--highlight-color)', 
+                    color: 'var(--button-color)',
+                    border: '1px solid black',
+                    borderRadius: '5px',
+                    cursor: 'pointer',
+                    transition: 'background-color 0.3s, box-shadow 0.3s',
+                    fontFamily: 'Caveat, cursive',
+                    fontSize: '18px',
+                    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+                    '&:hover': {
+                      backgroundColor: '#7BBAFF', 
+                      boxShadow: '0px 6px 12px rgba(0, 0, 0, 0.3)', 
+                    },
+                    textTransform: 'none',
+                  }}
+                >
+                  GET IN TOUCH
+                </Button>
+              </ScrollLink>
+            </Box>
           </CardContent>
         </Card>
       </Box>
       <div className="image-container">
-        <wired-image 
-          class="hero-image" 
-          src={heroImageSrc} 
-          alt="Hero Section" 
-          elevation="5" 
-          style={{ width: '300px', height: 'auto' }}
-        ></wired-image>
+        <div className={`blurred-img ${loaded ? 'loaded' : ''}`} style={{ backgroundImage: `url(${placeholderImageSrc})` }}>
+          <img
+            className={`hero-image ${loaded ? 'loaded' : ''}`}
+            src={heroImageSrc}
+            alt="Hero Section"
+            loading="lazy"
+            onLoad={handleImageLoad}
+          />
+        </div>
       </div>
     </section>
   );
